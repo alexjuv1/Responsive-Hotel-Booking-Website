1b16fce31f3a89d8daa3563bc39f767ef67c27b8
alexjuv1 <108079420+alexjuv1@users.noreply.github.com>
2022-11-23 19:24:32 +0200

    alexpull

--- main/views.py
+++ main/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import users, room
 # Create your views here.
 
 def index(response, id):
-    return HttpResponse("<h1>%s</h1>" %id)
+    a = users.objects.get(id=id)
+    return HttpResponse("<h1>%s</h1>" % a.first_name)
