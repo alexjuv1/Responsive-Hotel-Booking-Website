1d9f6a2eb56c3813b6ae2f8bb1eb8235818d0cc7
MohamadKreidieh <mkreidieh21@gmail.com>
2022-11-23 19:13:38 +0200

    mohamadtest

--- main/views.py
+++ main/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import users
 # Create your views here.
 
 def index(response, id):
-    return HttpResponse("<h1>%s</h1>" %id)
+    ls = users.objects.get(id=id)
+    return HttpResponse("<h1>%s</h1>" % ls.email)
