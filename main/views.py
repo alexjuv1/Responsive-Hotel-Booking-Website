af8abfbfdb4b4fcf70bb48a55866bbe1877719f4
MohamadKreidieh <mkreidieh21@gmail.com>
2022-11-24 12:52:25 +0200

    test

--- main/views.py
+++ main/views.py
@@ -3,10 +3,13 @@ from django.http import HttpResponse
 from .models import users
 # Create your views here.
 
+# def index(response, email, password):
+#     ls = users.objects.get(email == email and password == password)
 def index(response, id):
-    ls = users.objects.get(id=id)
+    ls = users.objects.get(id = id)
     #return HttpResponse("<h1>%s</h1>" % ls.first_name)
-    return render(response, "main/base.html", {})
+    return render(response, "main/viewUserData.html", {"ls": ls})
 
 def home(response):
+
     return render(response, "main/home.html", {})
