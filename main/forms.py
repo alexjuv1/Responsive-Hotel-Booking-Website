9785f1d5cbc8021da702c4175b60b314622d5583
alexjuv1 <108079420+alexjuv1@users.noreply.github.com>
2022-11-26 07:17:11 +0200

    reservation page changes

--- main/forms.py
+++ main/forms.py
@@ -1,5 +1,5 @@
 from django import forms
 
 class Reservation(forms.Form):
-    smoking = forms.BooleanField()
+    smoking = forms.CharField(label="Smoking", max_length=200)
     single = forms.CharField()
