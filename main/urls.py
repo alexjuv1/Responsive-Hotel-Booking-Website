c600c8ded2e4c66039ef9da2cc81e1f1fbe3ac2d
alexjuv1 <108079420+alexjuv1@users.noreply.github.com>
2022-11-27 21:58:51 +0200

    newest version of website

--- main/urls.py
+++ main/urls.py
@@ -3,15 +3,12 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("roomShow/roomShow/<int:id>", views.index, name="index"),
+    path("roomShow/<int:id>", views.index, name="index"),
     path("", views.home, name="home"),
-    path("create/", views.create, name="create"),
     path("profile/", views.profile, name="profile"),
     path("home/", views.home, name="home"),
-    path("create/", views.create, name="create"),
     path("view/", views.view, name="view"),
-    path("login/", ),
     path("reserve/", views.reserve, name="reserve"),
     path("reserve/roomShow/", views.roomShow, name="roomShow"),
-    path("reserve/roomShow/checkout", views.checkOut, name="checkOut")
+    path("roomShow/<int:id>/checkout/", views.checkOut, name="checkOut")
 ]
--- proj_351/urls.py
+++ proj_351/urls.py
@@ -22,7 +22,7 @@ from register import views as v
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include("Hotel Django.gfg.urls")),
+    path('', include("main.urls")),
     path("register/", v.register, name="register"),
     path('', include("django.contrib.auth.urls"))
 ]
