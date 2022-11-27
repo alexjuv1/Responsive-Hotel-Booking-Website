20e6c3bf458d5113e6843702992a6d3898dbbff4
alexjuv1 <108079420+alexjuv1@users.noreply.github.com>
2022-11-27 17:31:56 +0200

    same as last commit

--- proj_351/urls.py
+++ proj_351/urls.py
@@ -23,5 +23,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include("main.urls")),
     path("register/", v.register, name="register"),
-    path('register/templates/registration', include("django.contrib.auth.urls"))
+    path('', include("django.contrib.auth.urls"))
 ]
