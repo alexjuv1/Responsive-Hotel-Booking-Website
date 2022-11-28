497de7ddbc1558871126501fa62b6d7706720999
msalam21 <mohamadsalam03@gmail.com>
2022-11-28 09:38:39 +0200

    comit

--- Hotel Django/authentication/urls.py
+++ Hotel Django/authentication/urls.py
@@ -7,8 +7,7 @@ urlpatterns = [
     path ('signup' , views.signup, name ="signup"),
     path ('signin' , views.signin, name ="signin"),
     path ('signout' , views.signout, name ="signout"),
-    path ('homepage' , views.homepage, name ="homepage"),
+    path ('homepages' , views.homepage, name ="homepages"),
     path('book', views.book, name="book"),
     path ('activate/<uidb64>/<token>' , views.activate, name ="activate"),
-
 ]
